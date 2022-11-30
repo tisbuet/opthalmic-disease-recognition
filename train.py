8c50f2dd3f229f1136f8643671fb7fb5af03b3a3
tisbuet <tisbuet@gmail.com>
2022-11-30 21:55:37 +0600

    minor changes

--- train.py
+++ train.py
@@ -4,38 +4,22 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import cv2
 from tqdm import tqdm
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Activation, Dropout
-from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from sklearn.preprocessing import MinMaxScaler
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.utils import shuffle
-
-np.seterr(divide='ignore', invalid='ignore')
+from sklearn.model_selection import KFold
 
 seed = 7
 np.random.seed(seed)
-
-def k_Fold(x, n, i):
-    y_train_range = np.arange(np.ceil(i*len(x)*n/100),np.floor((i+1)*len(x)*n/100)).astype('int')
-    r = np.split(np.arange(len(x)), y_train_range)
-    X_train_range = np.append(r[0], r[-1])
-    return x[X_train_range], x[y_train_range]
-
-def dataHandle(X_train, Y_train):
-    a = np.nonzero(Y_train==1)[0]
-    b = np.nonzero(Y_train==0)[0][:len(a)]
-    x_train = np.vstack((X_train[a],X_train[b]))
-    y_train = np.vstack((np.ones(len(a))[:,None], np.zeros(len(b))[:,None]))
-    return x_train, y_train
-
 
 def SympIndexing(i):
     switcher={
@@ -67,7 +51,7 @@ count = 0
 for im in tqdm(image_names):
     image_path = os.path.join("ODIR-5K_Training_Images/ODIR-5K_Training_Dataset", im)
     bgr = cv2.imread(image_path)
-    bgr = cv2.resize(bgr, (40, 40))
+    bgr = cv2.resize(bgr, (32, 32))
     lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
     lab_planes = cv2.split(lab)
     clahe = cv2.createCLAHE(clipLimit=5.0)
@@ -75,10 +59,10 @@ for im in tqdm(image_names):
     lab = cv2.merge(lab_planes)
     bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
     a = bgr[:,:,1]
-    a = a/np.max(a)
+    a = a/255
     b.append(a)
 X_train = np.asarray(b)
-X_train = X_train.reshape(-1, 40, 40, 1).astype( 'float32' )
+X_train = X_train.reshape(-1, 32, 32, 1).astype('float32')
 
 print('Input taken......')
 
@@ -140,48 +124,48 @@ Y_train = np.asarray(y_train).astype('float32')
 
 def create_model(x, y):    
     model = Sequential()
-
-    model.add(Conv2D(32,kernel_size = (5,5), padding="same",input_shape=(x.shape[1:])))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(4, 4),strides=2, data_format=None))
-    model.add(Conv2D(128,kernel_size = (3,3),  padding="same"))
+    model.add(Conv2D(8,kernel_size = (3,3), padding="same",input_shape=(x.shape[1:]), dilation_rate=3))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2),strides=2, data_format=None))
-    model.add(Dropout(0.1))
+    
+    model.add(Dropout(0.25))
     model.add(Flatten())
-
-    model.add(Dense(128, activation= 'relu' ))
+    
+    model.add(Dense(16, activation= 'relu' ))
     model.add(Dropout(0.15))
-    model.add(Dense(y.shape[1], activation= 'sigmoid' ))
+    model.add(Dense(y.shape[1], activation='sigmoid'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=["accuracy"])
+    model.compile(loss='category_crossentropy', optimizer='adam', metrics=["accuracy"])
     return model
 
 file_path = 'best.h5'
-checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-early = EarlyStopping(monitor="val_acc", mode="max", patience=40)
+checkpoint = ModelCheckpoint(file_path, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+early = EarlyStopping(monitor="val_accuracy", mode="max", patience=40)
 tb = TensorBoard(log_dir='./', histogram_freq=1, write_graph=True, write_images=False)
 callbacks_list = [checkpoint, early, tb]
 aug = ImageDataGenerator(rotation_range=20, zoom_range=0.2,
     width_shift_range=0.2, height_shift_range=0.2, vertical_flip=True,horizontal_flip=True,                    
     fill_mode="nearest")
 
-k = 30
-
 x, y = shuffle(X_train, Y_train)
 
-for i in range(100//k):
+kf = KFold(n_splits=5)
 
-    t_x, val_x = k_Fold(x, k, i)
-    t_y, val_y = k_Fold(y, k, i)
+for train_index, test_index in kf.split(x):
+    t_x, val_x = x[train_index], x[test_index]
+    t_y, val_y = y[train_index], y[test_index]
+
     model = None
-    model = create_model(x, y)
-    results = model.fit(t_x, t_y, validation_data=(val_x, val_y), epochs=50, batch_size = 32, shuffle=True, verbose=2,
-                        callbacks = callbacks_list)
-
+    model = create_model(t_x, t_y)
+    
+    results = model.fit_generator(
+        aug.flow(t_x, t_y, batch_size=32),validation_data=(val_x, val_y),
+        steps_per_epoch=len(t_x)//32, epochs=50, callbacks = callbacks_list,
+        shuffle = True
+        )
 finemodel=create_model(x, y)
 finemodel.load_weights(file_path)
-for layer in finemodel.layers[:-4]:
+for layer in finemodel.layers[:-2]:
     layer.trainable = False
 finemodel.fit(t_x, t_y, validation_data=(val_x, val_y), epochs=100, batch_size = 32, shuffle=True, verbose=2,\
                         callbacks = [checkpoint, early])
